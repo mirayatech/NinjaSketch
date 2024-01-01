95ac4a2dade4741f0c2c30815280ea4e7998498f
〽️iraya <mirayaabrodi@gmail.com>
2024-01-01 14:07:03 +0100

    add: Interactive drawing functionality with mouse events and element types

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,107 @@
-import { useLayoutEffect } from "react";
+import { MouseEvent, useLayoutEffect, useState } from "react";
 import rough from "roughjs";
 
+type ElementType = {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  // TODO: add type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  roughElement: any;
+};
+
 export default function App() {
+  const [elements, setElements] = useState<ElementType[]>([]);
+  const [drawing, setDrawing] = useState(false);
+  const [elementType, setElementType] = useState<
+    "line" | "rectangle" | "circle"
+  >("line");
+
+  const generator = rough.generator();
+
+  const createElement = (
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number
+  ): ElementType => {
+    const roughElement =
+      elementType === "line"
+        ? generator.line(x1, y1, x2, y2)
+        : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+    return { x1, y1, x2, y2, roughElement };
+  };
+
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const context = canvas.getContext("2d") as CanvasRenderingContext2D;
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     const roughCanvas = rough.canvas(canvas);
-    const rect = roughCanvas.rectangle(10, 10, 200, 200);
-    roughCanvas.draw(rect);
-  });
+
+    elements.forEach(({ roughElement }) => {
+      roughCanvas.draw(roughElement);
+    });
+  }, [elements]);
+
+  const handleMouseDown = (event: MouseEvent<HTMLCanvasElement>) => {
+    setDrawing(true);
+    const { clientX, clientY } = event;
+    const element = createElement(clientX, clientY, clientX, clientY);
+    setElements((prevState) => [...prevState, element]);
+  };
+
+  const handleMouseMove = (event: MouseEvent<HTMLCanvasElement>) => {
+    if (!drawing) {
+      return;
+    }
+
+    const index = elements.length - 1;
+    const { clientX, clientY } = event;
+    const { x1, y1 } = elements[index];
+    const updateElement = createElement(x1, y1, clientX, clientY);
+
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updateElement;
+    setElements(elementsCopy);
+  };
+
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
 
   return (
     <div>
-      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight}>
-        Canvas
+      <div style={{ position: "fixed" }}>
+        <input
+          type="radio"
+          name="line"
+          id="line"
+          checked={elementType === "line"}
+          onChange={() => setElementType("line")}
+        />
+        <label htmlFor="line">line</label>
+
+        <input
+          type="radio"
+          name="rectangle"
+          id="rectangle"
+          checked={elementType === "rectangle"}
+          onChange={() => setElementType("rectangle")}
+        />
+
+        <label htmlFor="rectangle">rectangle</label>
+      </div>
+      <canvas
+        id="canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        Canvas{" "}
       </canvas>
     </div>
   );
