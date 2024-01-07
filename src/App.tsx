168b9c56341eab8dc911c792f0e893bb2f23540f
〽️iraya <mirayaabrodi@gmail.com>
2024-01-07 20:17:58 +0100

    refactor: Move shared functions to utilities.ts

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {
   useState,
 } from "react";
 import rough from "roughjs";
-import getStroke from "perfect-freehand";
 import { useHistory } from "./hooks/useHistory";
 import { usePressedKeys } from "./hooks/usePressedKeys";
 import {
@@ -14,7 +13,16 @@ import {
   SelectedElementType,
   ExtendedElementType,
   ElementType,
-} from "./types";
+} from "./library/types";
+import {
+  cursorForPosition,
+  createElement,
+  getElementAtPosition,
+  resizedCoordinates,
+  adjustElementCoordinates,
+  drawElement,
+  adjustmentRequired,
+} from "./library/utilities";
 
 export default function App() {
   const { elements, setElements, undo, redo } = useHistory([]);
@@ -28,277 +36,8 @@ export default function App() {
   const [selectedElement, setSelectedElement] = useState<ElementType | null>();
   const [scale, setScale] = useState(1);
   const [scaleOffset, setScaleOffset] = useState({ x: 0, y: 0 });
-  const generator = rough.generator();
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const pressedKeys = usePressedKeys();
-  const cursorForPosition = (position: string) => {
-    switch (position) {
-      case "topLeft":
-      case "bottomRight":
-        return "nwse-resize";
-      case "topRight":
-      case "bottomLeft":
-        return "nesw-resize";
-      case "start":
-      case "end":
-        return "move";
-      case "inside":
-        return "move";
-      default:
-        return "default";
-    }
-  };
-
-  const resizedCoordinates = (
-    clientX: number,
-    clientY: number,
-    position: string,
-    coordinates: { x1: number; y1: number; x2: number; y2: number }
-  ) => {
-    const { x1, y1, x2, y2 } = coordinates;
-
-    switch (position) {
-      case "start":
-      case "topLeft":
-        return {
-          x1: clientX,
-          y1: clientY,
-          x2,
-          y2,
-        };
-      case "topRight":
-        return {
-          x1,
-          y1: clientY,
-          x2: clientX,
-          y2,
-        };
-      case "bottomLeft":
-        return {
-          x1: clientX,
-          y1,
-          x2,
-          y2: clientY,
-        };
-      case "end":
-      case "bottomRight":
-        return {
-          x1,
-          y1,
-          x2: clientX,
-          y2: clientY,
-        };
-      default:
-        return coordinates;
-    }
-  };
-
-  const createElement = (
-    id: number,
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    type: Tools
-  ): ElementType => {
-    switch (type) {
-      case Tools.Line:
-      case Tools.Rectangle: {
-        const roughElement =
-          type === Tools.Line
-            ? generator.line(x1, y1, x2, y2)
-            : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-        return { id, x1, y1, x2, y2, type, roughElement };
-      }
-      case Tools.Pencil: {
-        const defaultRoughElement = null;
-        return {
-          id,
-          x1: 0,
-          y1: 0,
-          x2: 0,
-          y2: 0,
-          type,
-          points: [{ x: x1, y: y1 }],
-          roughElement: defaultRoughElement,
-        };
-      }
-      case Tools.Text:
-        return { id, type, x1, y1, x2, y2, text: "" };
-      default:
-        throw new Error(`Type not recognised: ${type}`);
-    }
-  };
-
-  type Point = { x: number; y: number };
-
-  const distance = (a: Point, b: Point) =>
-    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
-
-  const nearPoint = (
-    x: number,
-    y: number,
-    x1: number,
-    y1: number,
-    name: string
-  ) => {
-    return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
-  };
-
-  const onLine = (
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    x: number,
-    y: number,
-    maxDistance: number = 1
-  ): string | null => {
-    const a: Point = { x: x1, y: y1 };
-    const b: Point = { x: x2, y: y2 };
-    const c: Point = { x, y };
-    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
-    return Math.abs(offset) < maxDistance ? "inside" : null;
-  };
-
-  const positionWithinElement = (
-    x: number,
-    y: number,
-    element: ElementType
-  ) => {
-    const { type, x1, x2, y1, y2 } = element;
-    switch (type) {
-      case Tools.Line: {
-        const on = onLine(x1, y1, x2, y2, x, y);
-        const start = nearPoint(x, y, x1, y1, "start");
-        const end = nearPoint(x, y, x2, y2, "end");
-        return start || end || on;
-      }
-      case Tools.Rectangle: {
-        const topLeft = nearPoint(x, y, x1, y1, "topLeft");
-        const topRight = nearPoint(x, y, x2, y1, "topRight");
-        const bottomLeft = nearPoint(x, y, x1, y2, "bottomLeft");
-        const bottomRight = nearPoint(x, y, x2, y2, "bottomRight");
-        const inside =
-          x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
-        return topLeft || topRight || bottomLeft || bottomRight || inside;
-      }
-      case Tools.Pencil: {
-        const betweenAnyPoint = element.points!.some((point, index) => {
-          const nextPoint = element.points![index + 1];
-          if (!nextPoint) return false;
-          return (
-            onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null
-          );
-        });
-        return betweenAnyPoint ? "inside" : null;
-      }
-      case Tools.Text:
-        return x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
-      default:
-        throw new Error(`Type not recognised: ${type}`);
-    }
-  };
-
-  const adjustElementCoordinates = (element: ElementType) => {
-    const { type, x1, y1, x2, y2 } = element;
-
-    if (type === Tools.Rectangle) {
-      const minX = Math.min(x1, x2);
-      const maxX = Math.max(x1, x2);
-      const minY = Math.min(y1, y2);
-      const maxY = Math.max(y1, y2);
-      return { x1: minX, y1: minY, x2: maxX, y2: maxY };
-    } else {
-      if (x1 < x2 || (x1 === x2 && y1 < y2)) {
-        return { x1, y1, x2, y2 };
-      } else {
-        return { x1: x2, y1: y2, x2: x1, y2: y1 };
-      }
-    }
-  };
-
-  const getElementAtPosition = (
-    x: number,
-    y: number,
-    elements: ElementType[]
-  ) => {
-    return elements
-      .map((element) => ({
-        ...element,
-        position: positionWithinElement(x, y, element),
-      }))
-      .find((element) => element.position !== null);
-  };
-
-  const getSvgPathFromStroke = (stroke: [number, number][]) => {
-    if (!stroke.length) return "";
-
-    const d = stroke.reduce(
-      (
-        acc: string[],
-        [x0, y0]: [number, number],
-        i: number,
-        arr: [number, number][]
-      ) => {
-        const [x1, y1] = arr[(i + 1) % arr.length];
-        acc.push(
-          x0.toString(),
-          y0.toString(),
-          ((x0 + x1) / 2).toString(),
-          ((y0 + y1) / 2).toString()
-        );
-        return acc;
-      },
-      ["M", ...stroke[0].map((num) => num.toString()), "Q"]
-    );
-
-    d.push("Z");
-    return d.join(" ");
-  };
-
-  const drawElement = (
-    // TODO: add type
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    roughCanvas: any,
-    context: CanvasRenderingContext2D,
-    element: ElementType
-  ) => {
-    switch (element.type) {
-      case "line":
-      case "rectangle":
-        roughCanvas.draw(element.roughElement);
-        break;
-      case "pencil": {
-        if (!element.points) {
-          throw new Error("Pencil element points are undefined");
-        }
-        const strokePoints = getStroke(element.points);
-        const formattedPoints: [number, number][] = strokePoints.map(
-          (point) => {
-            if (point.length !== 2) {
-              throw new Error(
-                `Expected point to have exactly 2 elements, got ${point.length}`
-              );
-            }
-            return [point[0], point[1]];
-          }
-        );
-        const stroke = getSvgPathFromStroke(formattedPoints);
-        context.fill(new Path2D(stroke));
-        break;
-      }
-      case "text": {
-        context.textBaseline = "top";
-        context.font = "24px sans-serif";
-        const text = element.text || "";
-        context.fillText(text, element.x1, element.y1);
-        break;
-      }
-      default:
-        throw new Error(`Type not recognised: ${element.type}`);
-    }
-  };
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -427,9 +166,6 @@ export default function App() {
     }
     setElements(elementsCopy, true);
   };
-
-  const adjustmentRequired = (type: Tools) =>
-    ["line", "rectangle"].includes(type);
 
   const getMouseCoordinates = (event: MouseEvent) => {
     const clientX =
