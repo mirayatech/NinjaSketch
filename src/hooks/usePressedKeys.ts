cfce910c5199e71efce07c53ac1760e487b694d5
〽️iraya <mirayaabrodi@gmail.com>
2024-01-07 16:10:20 +0100

    feat: create usePressedKeys hook

---
+++ src/hooks/usePressedKeys.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export const usePressedKeys = () => {
+  const [pressedKeys, setPressedKeys] = useState<Set<string>>(new Set());
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      setPressedKeys((prevKeys) => new Set(prevKeys).add(event.key));
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setPressedKeys((prevKeys) => {
+        const updatedKeys = new Set(prevKeys);
+        updatedKeys.delete(event.key);
+        return updatedKeys;
+      });
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return pressedKeys;
+};
