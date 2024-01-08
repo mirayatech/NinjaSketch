5ce57a68186ee2be6de39fa7c6dc190e3926f75e
〽️iraya <mirayaabrodi@gmail.com>
2024-01-08 09:07:37 +0100

    feat: create ActionBar component

---
+++ src/components/action-bar/action-bar.tsx
@@ -0,0 +1,37 @@
+import { Tools } from "../../library/types";
+
+import { LuMousePointer, LuPencil } from "react-icons/lu";
+import { FiSquare } from "react-icons/fi";
+import { IoText } from "react-icons/io5";
+import { PiMinusBold } from "react-icons/pi";
+import "./action-bar-style.css";
+
+type ActionBarProps = {
+  tool: Tools;
+  setTool: (tool: Tools) => void;
+};
+
+export function ActionBar({ tool, setTool }: ActionBarProps) {
+  return (
+    <div className="actionBar">
+      {Object.values(Tools).map((t, index) => (
+        <div className={`inputWrapper ${tool === t ? "selected" : ""}`} key={t}>
+          <input
+            type="radio"
+            id={t}
+            checked={tool === t}
+            onChange={() => setTool(t)}
+          />
+          <label htmlFor={t}>{t}</label>
+          {t === "selection" && <LuMousePointer />}
+          {t === "rectangle" && <FiSquare />}
+          {t === "line" && <PiMinusBold />}
+          {t === "pencil" && <LuPencil />}
+          {t === "text" && <IoText />}
+
+          <span>{index + 1}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
